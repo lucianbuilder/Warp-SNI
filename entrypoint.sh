ebb82a84bb276251706bc0cc8324f0f79c590764
lucianbuilder <129403288+lucianbuilder@users.noreply.github.com>
2025-07-20 09:51:01 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # exit when any command fails
-#set -e
+set -e
 
 # create a tun device if not exist to ensure compatibility with Podman
-if [ ! -e /dev/net/tun ]; then
-    sudo mkdir -p /dev/net
-    sudo mknod /dev/net/tun c 10 200
-    sudo chmod 600 /dev/net/tun
-fi
+#if [ ! -e /dev/net/tun ]; then
+#    sudo mkdir -p /dev/net
+#    sudo mknod /dev/net/tun c 10 200
+#    sudo chmod 600 /dev/net/tun
+#fi
 
 # start dbus
 sudo mkdir -p /run/dbus
@@ -21,20 +21,21 @@ sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 sudo warp-svc --accept-tos &
 
 # sleep to wait for the daemon to start, default 2 seconds
-sleep "$WARP_SLEEP"
+#sleep "$WARP_SLEEP"
+sleep 30s
 
 # if /var/lib/cloudflare-warp/reg.json not exists, setup new warp client
 if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
     # if /var/lib/cloudflare-warp/mdm.xml not exists or REGISTER_WHEN_MDM_EXISTS not empty, register the warp client
     if [ ! -f /var/lib/cloudflare-warp/mdm.xml ] || [ -n "$REGISTER_WHEN_MDM_EXISTS" ]; then
-        sudo warp-cli registration new && echo "Warp client registered!"
+        sudo warp-cli --accept-tos registration new && echo "Warp client registered!"
         # if a license key is provided, register the license
         if [ -n "$WARP_LICENSE_KEY" ]; then
             echo "License key found, registering license..."
-            sudo warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+            sudo warp-cli --accept-tos registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
         fi
-    sudo warp-cli tunnel protocol set MASQUE
-    sudo warp-cli mode proxy
+    sudo warp-cli --accept-tos tunnel protocol set MASQUE
+    sudo warp-cli --accept-tos mode proxy
     fi
     # connect to the warp server
     sudo warp-cli --accept-tos connect
